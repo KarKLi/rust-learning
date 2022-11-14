df757b3ba3b64e8401924f1b222a69b3f26713ad
karkli <kark_li@qq.com>
2022-11-14 19:21:27 +0800

    update

--- learn_2_8_4_trait_other_feature/src/main.rs
+++ learn_2_8_4_trait_other_feature/src/main.rs
@@ -137,4 +137,102 @@ fn main() {
             }
         }
     }
+
+    // 如何调用同名的方法
+    {
+        // 不同特征拥有同名方法很正常
+        trait Pilot {
+            fn fly(&self);
+        }
+
+        trait Wizard {
+            fn fly(&self);
+        }
+
+        struct Human;
+
+        impl Pilot for Human {
+            fn fly(&self) {
+                println!("pilot")
+            }
+        }
+
+        impl Wizard for Human {
+            fn fly(&self) {
+                println!("wizard")
+            }
+        }
+
+        impl Human {
+            fn fly(&self) {
+                println!("human")
+            }
+        }
+
+        // 如果不加任何限制，默认调用的是方法
+        let person = Human;
+        person.fly(); // Human.fly()
+
+        // 或者直接调用特征里的方法，传递&self参数
+        Pilot::fly(&person);
+        Wizard::fly(&person);
+
+        // 但如果特征里的方法没有&self参数呢（比如new）？
+        // 这里需要用完全限定语法
+        <Human as Pilot>::fly(&person);
+        <Human as Wizard>::fly(&person);
+        // 语法：<Type as trait>::method(params)
+    }
+
+    // 特征定义中的特征约束
+    {
+        // 你可以在定义特征的时候约束实现这个特征的结构体的前提
+        // 比如必须要求这个结构体先实现什么特征
+        use std::fmt::Display;
+
+        trait OutlinePrint: Display {
+            fn outline_print(&self) {
+                let output = self.to_string(); // 因为实现了OutlinePrint特征的结构体必须先实现了Display，进而实现了ToString
+                let len = output.len();
+                println!("{}", "*".repeat(len + 4));
+                println!("*{}*", " ".repeat(len + 2));
+                println!("* {} *", output);
+                println!("*{}*", " ".repeat(len + 2));
+                println!("{}", "*".repeat(len + 4));
+            }
+        }
+
+        // 假设你没有实现Display就去实现OutlinePrint
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        // impl OutlinePrint for Point {} // invalid!
+        use std::fmt;
+        impl Display for Point {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{} {}", self.x, self.y)
+            }
+        }
+        impl OutlinePrint for Point {} // valid!
+    }
+
+    // 如何绕过孤儿规则（不能在非结构体/特征的作用域为某个结构体声明某个特征）
+    {
+        use std::fmt;
+
+        // struct接个元组把某个类型wrap起来，这样这个struct就在自己的作用域了
+        // 因此就可以破坏孤儿规则，在当前的作用域为某个外部类型加上某个外部特征了
+        struct Wrapper(Vec<String>);
+
+        impl fmt::Display for Wrapper {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "[{}]", self.0.join(", "))
+            }
+        }
+
+        let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+        println!("w = {}", w);
+    }
 }
